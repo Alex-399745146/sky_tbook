44832f90ca6b78e4b0f39a2d72ae97956e1d1d80
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-07-05 01:26:00 +0300

    task_1-2 over

--- app_materials/admin.py
+++ app_materials/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+# app_materials/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Course, Lesson
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'course')
+    list_filter = ('course',)
+    search_fields = ('name',)
--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'app_users.User'
 
 REST_FRAMEWORK = {
 
