08b4132625fa81304f3fa2040abfa243e6e4c6ef
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-07-05 22:53:47 +0300

    finish homework_30_1

--- app_materials/models.py
+++ app_materials/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название')
-    preview = models.ImageField(upload_to='course_previews/', verbose_name='Превью', null=True, blank=True)
-    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    name = models.CharField(max_length=255, verbose_name="Название")
+    preview = models.ImageField(upload_to="course_previews/", verbose_name="Превью", null=True, blank=True)
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -16,13 +16,13 @@ class Lesson(models.Model):
     course = models.ForeignKey(
         Course,
         on_delete=models.CASCADE,
-        related_name='lessons',
-        verbose_name='Курс',
+        related_name="lessons",
+        verbose_name="Курс",
     )
-    name = models.CharField(max_length=255, verbose_name='Название')
-    description = models.TextField(verbose_name='Описание', blank=True, null=True)
-    preview = models.ImageField(upload_to='lesson_previews/', verbose_name='Превью', null=True, blank=True)
-    video_url = models.CharField(max_length=255, verbose_name='Ссылка на видео')
+    name = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    preview = models.ImageField(upload_to="lesson_previews/", verbose_name="Превью", null=True, blank=True)
+    video_url = models.CharField(max_length=255, verbose_name="Ссылка на видео")
 
     def __str__(self):
         return self.name
--- app_materials/serializers.py
+++ app_materials/serializers.py
@@ -1,13 +1,14 @@
 # app_materials/serializers.py
 
 from rest_framework import serializers
+
 from .models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -15,4 +16,4 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = "__all__"
