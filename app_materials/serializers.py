f35ad5e0a79c045793001d978771f846b92971cd
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-07-08 19:45:55 +0300

    task_1 over

--- app_materials/serializers.py
+++ app_materials/serializers.py
@@ -13,7 +13,18 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(many=True, read_only=True)
+    lessons_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = (
+            'name',
+            'preview',
+            'description',
+            # добавленое динамическое поле
+            'lessons_count',
+        )
+
+    def get_lessons_count(self, obj):
+        # obj — это конкретный Course
+        return obj.lessons.count()
