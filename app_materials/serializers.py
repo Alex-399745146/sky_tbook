8b0ad1668e41f507879eee3b724d9c1807351803
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-07-16 23:01:16 +0300

    task_1 over, add validaters check URL points

--- app_materials/serializers.py
+++ app_materials/serializers.py
@@ -3,9 +3,16 @@
 from rest_framework import serializers
 
 from .models import Course, Lesson
+from .validators import validate_video_url
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    video_url = serializers.CharField(
+        max_length=255,
+        validators=[validate_video_url],  # Подключение к модулю с кастомным валидатором.
+        help_text="Ссылка на видео с youtube.com",
+    )
+
     class Meta:
         model = Lesson
         fields = (
